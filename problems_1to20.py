2f867afd16af680717d797944f75aa46bb75f8be
Clément Savalle <savalle.clement@gmail.com>
2021-03-26 09:46:18 +0000

    problem 23

--- problems_1to20.py
+++ problems_1to20.py
@@ -452,6 +452,31 @@ def problem22():
     print("Sol22: ", solution22)
     return solution22
 
+def problem23():
+    """Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers."""
+
+    rangemax = 28123
+    abundant_numbers = get_abundant_numbers(rangemax)
+    print(abundant_numbers)
+
+    # list_not_sum_abundants = set()
+
+    # for n in range(1,rangemax):
+    #     if all([not(n-a in abundant_numbers) for a in abundant_numbers]):
+    #         list_not_sum_abundants.add(n)
+
+    not_sum_abundants = [*range(1, rangemax+1)]
+    solution23 = 0
+    for a in abundant_numbers:
+        for b in abundant_numbers:
+            if a+b in not_sum_abundants:
+                not_sum_abundants.remove(a+b)
+
+    print(not_sum_abundants)
+    for n in not_sum_abundants:
+        solution23 += n
+
+    print("Sol23: ", solution23)
 
 def problem67():
     """Find the maximum total from top to bottom of the triangle in triangle67.txt."""
